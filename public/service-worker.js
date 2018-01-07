54a18836fdf054d3164f16c67afa49c9a823486e
Bogdan Lupu <bogdanlupu13@yahoo.ro>
2018-01-07 02:10:55 +0200

    Added service worker - WIP

---
+++ public/service-worker.js
@@ -0,0 +1,54 @@
+// importScripts('/cache-polyfill.js');
+self.addEventListener('install', function(e) {
+    e.waitUntil(
+        caches.open('asle').then(function(cache) {
+            console.log("is this working?");
+            return cache.addAll([
+                '/html/index.html',
+                '/html/about.html',
+                '/html/score.html',
+                '/css/about.css',
+                '/css/index.css',
+                '/css/score.css',
+                '/css/font-awesome.min.css',
+                '/img/favicon-96x96.png',
+                '/img/favicon-32x32.png',
+                '/img/favicon-16x16.png',
+                '/img/japanese.jpg',
+                '/img/level1.png',
+                '/img/level2.png',
+                '/img/level3.png',
+                '/img/level4.png',
+                '/img/logo.png',
+                '/img/logo2.png',
+                '/img/overlay.png',
+                '/img/pic05.jpg',
+                '/img/tsunami.jpg',
+                '/js/DataService.js',
+                '/js/ModuleLoader.js',
+                '/js/score.js',
+                '/js/score2.js',
+                '/components/draw/draw.html',
+                '/components/draw/draw.css',
+                '/components/draw/draw.js',
+                '/components/quiz/quiz.html',
+                '/components/quiz/quiz.css',
+                '/components/quiz/quiz.js',
+                'comp.json',
+                'https://fonts.googleapis.com/css?family=Roboto:100,100i,300,300i,400,500,700'
+            ]);
+        })
+    );
+});
+
+self.addEventListener('fetch', function(event) {
+    event.respondWith(
+        caches.match(event.request).then(function(response) {
+            return response || fetch(event.request)
+                .catch(function(event) {
+                    console.log("Could not fetch data: ", event);
+                    // return caches.match(event.request);;
+            })
+        })
+    );
+});
